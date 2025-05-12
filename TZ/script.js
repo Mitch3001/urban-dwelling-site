8f5c624dd6e878b22958152169d6cafcc3d67091
Mitch3001 <mihailkorhy@gmail.com>
2025-05-12 19:39:26 +0300

    Add files via upload

---
+++ TZ/script.js
@@ -0,0 +1,6 @@
+// Example script for handling interactions (if needed)
+document.querySelectorAll('.action-btn').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Вы добавили товар в корзину!');
+    });
+  });
